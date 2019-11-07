bd7fb6ab9419f312fc3e1ec24d6aace06fa59f8c
David Álvarez Rosa <david@alvarezrosa.com>
2019-11-07 19:50:57 +0100

    More.

---
+++ errores/compara.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Los siete errores - David Álvarez</title>
+  </head>
+
+  <body background="../img/background.jpg">
+	<h1 align="center" style="margin-left: 40px; margin-right: 40px;">Los siete errores - David Álvarez</h1>
+	<a href="../index.html">
+	    <img src="../img/home.png" height="30px" style="position: absolute; top: 22px; left: 15px;" />
+	</a>
+    
+    <br><br><br>
+    <p align="center">Página en construcción.</p>
+  </body>
+</html>
